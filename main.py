b98b90137265164fc89904fd22f9dfa643da29db
Udit <uditmishra.um@gmail.com>
2022-03-03 21:12:51 +0530

    Password generator function and few bug fixes.

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+import random
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
 
 # ---------------------------- CONSTANTS  ---------------------------------- #
 CHARBLACK = "#203239"
-FONT_NAME = "Calibri"
+FONT_NAME = "Calibre"
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+def generate_password():
+    pwd_entry.delete(0, END)  # Deletes the previous entry and does not allow new password to append to last one.
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    letters_list = [choice(letters) for each in range(randint(8, 10))]
+    numbers_list = [choice(numbers) for each in range(randint(2, 4))]
+    symbols_list = [choice(symbols) for each in range(randint(2, 4))]
+
+    password_list = letters_list + numbers_list + symbols_list
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    pwd_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -21,7 +46,7 @@ def save():
             with open("myinfo.txt", "a") as data:
                 data.write(f"{website} | {email} | {pwd}\n")
                 website_entry.delete(0, END)
-                pwd_entry.delete(0, END)
+                pwd_entry.delete(0, END)  # Deletes the previous entry
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -37,7 +62,7 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
 # Labels
-website_label = Label(text="Website:", fg=CHARBLACK,)
+website_label = Label(text="Website:", fg=CHARBLACK, )
 website_label.grid(column=0, row=1)
 
 email_label = Label(text="Email/Username:", fg=CHARBLACK, )
@@ -59,10 +84,10 @@ pwd_entry = Entry(width=25)
 pwd_entry.grid(column=1, row=3)
 
 # Button
-generate_button = Button(text="Generate Password", width=20)
-generate_button.grid(column=2, row=3, sticky= "nw")
+generate_button = Button(text="Generate Password", width=20, command=generate_password)
+generate_button.grid(column=2, row=3, sticky="nw")
 
-add_button = Button(text="Add", fg=CHARBLACK, width=43, command= save)
+add_button = Button(text="Add", fg=CHARBLACK, width=43, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
