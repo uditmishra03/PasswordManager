9730b2417bfc17618e4c19a69eac918af1fec462
Udit <uditmishra.um@gmail.com>
2022-03-05 20:04:27 +0530

    Added search feature for website search. Exception handling has been added for search operation and file handling.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 # ---------------------------- CONSTANTS  ---------------------------------- #
 CHARBLACK = "#203239"
@@ -31,22 +31,60 @@ def generate_password():
     pyperclip.copy(password)
 
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title=f"Error", message=f"No Data File found.")
+    else:
+        if website not in data:
+            messagebox.showinfo(title=f"{website}", message=f"No details for the {website} exists.")
+        else:
+            email = data[website]['email']
+            password = data[website]['password']
+
+            messagebox.showinfo(title=f"{website}", message=f"Email: {email} \nPassword: {password}")
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     email = email_entry.get()
     pwd = pwd_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": pwd,
+        }
+    }
 
     if len(website) == 0 or len(pwd) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty")
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered: \nEmail: {email} \nPassword={pwd} \nIs it ok to save?")
-        if is_ok:
-            with open("myinfo.txt", "a") as data:
-                data.write(f"{website} | {email} | {pwd}\n")
-                website_entry.delete(0, END)
-                pwd_entry.delete(0, END)  # Deletes the previous entry
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        except json.decoder.JSONDecodeError:
+            with open('data.json', 'w') as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating the old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving the update d data
+                json.dump(data, data_file, indent=4)
+
+        finally:
+            website_entry.delete(0, END)
+            pwd_entry.delete(0, END)  # Deletes the previous entry
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -72,8 +110,8 @@ pwd_label = Label(text="Password:", fg=CHARBLACK)
 pwd_label.grid(column=0, row=3)
 
 # Entry
-website_entry = Entry(width=50)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=25)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
 email_entry = Entry(width=50)
@@ -84,10 +122,13 @@ pwd_entry = Entry(width=25)
 pwd_entry.grid(column=1, row=3)
 
 # Button
-generate_button = Button(text="Generate Password", width=20, command=generate_password)
+search_button = Button(text="Search", width=20, command=find_password)
+search_button.grid(column=2, row=1, sticky="nw")
+
+generate_button = Button(text="Generate Password", width=20, command=generate_password, )
 generate_button.grid(column=2, row=3, sticky="nw")
 
-add_button = Button(text="Add", fg=CHARBLACK, width=43, command=save)
+add_button = Button(text="Add", fg=CHARBLACK, width=43, command=save, highlightthickness=0)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
